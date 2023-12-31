4e69dabe4d1aeb4bd33d7e66e64cadbeef30f645
Sean Uz <cuz3@gatech.edu>
2023-12-30 21:00:55 -0500

    Add slider and make grid changeable

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
+function constructGrid(gridSize) {
+    const grid = document.createElement("div");
+    let boxSize = gridContainer.offsetWidth/gridSize;
+
+    grid.className = "grid";
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement("div");
+        row.className = "gridrow";
+        for (let j = 0; j < gridSize; j++) {
+            const box = document.createElement("div");
+            box.className = "gridbox";
+            box.style.width = boxSize + "px";
+            box.style.height = boxSize + "px";
+            box.style.backgroundColor = "black";
+            box.addEventListener("mouseenter", colorBox);
+            row.appendChild(box);
+        }
+        grid.appendChild(row);
+    }
+    gridContainer.appendChild(grid);
+}
+
 function colorBox(event) {
     event.target.style.backgroundColor = "red";
 }
 
-const GRID_SIZE = 16;
-
-for (let i = 0; i < GRID_SIZE; i++) {
-    const row = document.createElement("div");
-    row.className = "gridrow";
-    for (let j = 0; j < GRID_SIZE; j++) {
-        const box = document.createElement("div");
-        box.className = "gridbox";
-        box.addEventListener("mouseenter", colorBox);
-        row.appendChild(box);
-    }
-
-    document.querySelector(".grid").appendChild(row);
+function changeGrid(event) {
+    gridContainer.removeChild(document.querySelector(".grid"));
+    constructGrid(this.value);
+    document.querySelector(".gridSize").textContent = this.value;
 }
 
+
+
+const gridContainer = document.querySelector(".gridContainer");
+constructGrid(16);
+
+const gridSizeSelector = document.querySelector(".changeGrid");
+gridSizeSelector.addEventListener("input", changeGrid);
