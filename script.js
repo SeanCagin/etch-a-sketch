ad6a2cd1db2edf1c6d271e3e69bf555751dc048c
Sean Uz <cuz3@gatech.edu>
2023-12-30 20:21:34 -0500

    Impletement color change on hover

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
+function colorBox(event) {
+    event.target.style.backgroundColor = "red";
+}
+
 const GRID_SIZE = 16;
 
 for (let i = 0; i < GRID_SIZE; i++) {
-    let row = document.createElement("div");
+    const row = document.createElement("div");
     row.className = "gridrow";
     for (let j = 0; j < GRID_SIZE; j++) {
-        let box = document.createElement("div");
+        const box = document.createElement("div");
         box.className = "gridbox";
+        box.addEventListener("mouseenter", colorBox);
         row.appendChild(box);
     }
 
     document.querySelector(".grid").appendChild(row);
 }
 
-document.createElement("div");
